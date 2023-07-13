6ee1a6907b54eb28d66d1697585b3716a8ac6dd6
Julia Rubina <jl_rubina@mail.ru>
2023-07-13 21:26:51 +0300

    fix

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,19 +1,14 @@
 function compareArrays(arr1, arr2) {
-    if (arr1.length === arr2.length) {
-        return arr1.every((item, i) => item === arr2[i]);
-    }
-    else {
-        return false;
-    }  
+	return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let result = users.filter(user => user.gender === gender).map(users => users.age).reduce((acc, item, index, arr) => {
-        acc += item;
-        if (index === arr.length - 1) {
-            return acc / arr.length;
-        }
-        return acc;
-    }, 0)
-    return result;
+	let result = users.filter(user => user.gender === gender).map(users => users.age).reduce((acc, item, index, arr) => {
+		acc += item;
+		if (index === arr.length - 1) {
+			return acc / arr.length;
+		}
+		return acc;
+	}, 0)
+	return result;
 }
