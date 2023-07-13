741aeb74d228d3e100e1815151dc2ce08225fb75
Julia Rubina <jl_rubina@mail.ru>
2023-07-13 21:15:20 +0300

    Третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+        return arr1.every((item, i) => item === arr2[i]);
+    }
+    else {
+        return false;
+    }  
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender).map(users => users.age).reduce((acc, item, index, arr) => {
+        acc += item;
+        if (index === arr.length - 1) {
+            return acc / arr.length;
+        }
+        return acc;
+    }, 0)
+    return result;
 }
